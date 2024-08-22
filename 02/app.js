5c8a987151a12d78f6e8c8797c8eb94e977b3f69
Say.vi <say.julia.vi@gmail.com>
2024-08-22 23:28:07 +0200

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,18 @@
 console.log('DOM');
+
+const dataUrl = document.querySelector('[data-url]');
+const linkFromHtml = dataUrl.dataset.url;
+const hrefTag = dataUrl.setAttribute("href", linkFromHtml);
+
+
+let dataUrlArray = document.querySelectorAll('[data-url]');
+
+console.log(dataUrlArray)
+console.log(dataUrlArray[0].dataset.url)
+
+dataUrlArray.forEach(element=> {
+    const url = element.dataset.url;
+    element.setAttribute('href', url)
+});
+
+console.log(dataUrlArray)
